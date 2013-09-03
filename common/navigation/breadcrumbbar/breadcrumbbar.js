d40941344f545bdc921ec199b2bddcc08ad93e5d
Craig <cmcnamara87@gmail.com>
2013-09-03 16:28:31 +1000

    Small stuff with the breadcrumb part.

--- common/navigation/breadcrumbbar/breadcrumbbar.js
+++ common/navigation/breadcrumbbar/breadcrumbbar.js
@@ -1,7 +1,7 @@
 angular.module('directives.navigation.breadcrumbbar', [])
 
 // A simple directive to display a gravatar image given an email
-    .directive('breadcrumbbar', ['$location', 'PatientModel', function ($location, PatientModel) {
+    .directive('breadcrumbbar', ['$location', 'PatientModel', 'AuthService', function ($location, PatientModel, AuthService) {
         return {
             restrict: 'E',
             template: '<div class="breadcrumbbar"><span ng-bind-html-unsafe="breadcrumbs"></span></div>',
@@ -22,11 +22,24 @@ angular.module('directives.navigation.breadcrumbbar', [])
                     } else if(parts[0] == "patient") {
                         if(parts[2] == "share") {
                             var myPatient = PatientModel.get({id: parts[1]}, function(patient) {
-                                $scope.breadcrumbs = "<a href='#/patients/my-patients'>My Patients<a/> > <a href='#/patient/" + patient.id + "/summary-dashboard'>" + patient.first_name + " " + patient.last_name + "</a> > <a href='#/patient/" + patient.id + "/community'>Community<a/>";
+                                if(patient.uid == AuthService.getUser().uid) {
+                                    var root = "<a href='#/patients/my-patients'>My Patients<a/>";
+                                } else {
+                                    var root = "<a href='#/patients/my-patients'>My Groups<a/>";
+                                }
+
+                                $scope.breadcrumbs = root + " > <a href='#/patient/" + patient.id + "/summary-dashboard'>" + patient.first_name + " " + patient.last_name + "</a> > <a href='#/patient/" + patient.id + "/community'>Community<a/>";
                             });
                         } else {
                             var myPatient = PatientModel.get({id: parts[1]}, function(patient) {
-                                $scope.breadcrumbs = "<a href='#/patients/my-patients'>My Patients<a/> > <a href='#/patient/" + patient.id + "/summary-dashboard'>" + patient.first_name + " " + patient.last_name + "</a>";
+                                if(patient.uid == AuthService.getUser().uid) {
+                                    var root = "<a href='#/patients/my-patients'>My Patients<a/>";
+                                } else {
+                                    var root = "<a href='#/patients/my-patients'>My Groups<a/>";
+                                }
+
+                              
+                                $scope.breadcrumbs = root + " > <a href='#/patient/" + patient.id + "/summary-dashboard'>" + patient.first_name + " " + patient.last_name + "</a>";
                             });
                         }
                     }
