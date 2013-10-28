5b1fb38179c02361257be120336d13f9dd9c27eb
Craig <cmcnamara87@gmail.com>
2013-10-28 16:35:28 +1000

    Activity feed now pulls in and displays activity events the user has access to

--- app/feed/feed.js
+++ app/feed/feed.js
@@ -2,38 +2,34 @@ angular.module('feed', [])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/feed', {
-            templateUrl:'app/feed/feed.tpl.html',
-            controller:'FeedCtrl',
-            resolve:{
-//                patients: ['PatientModel', 'AuthService', '$q', 'Param', function (PatientModel, auth, $q, Param) {
-//
-//                    var defer = $q.defer();
-//
-//                    console.log("hello world");
-//                    auth.checkCurrentUser().then(function() {
-//                        console.log("got current user");
-//                        var patients = PatientModel.index({
-//                            'uid': auth.getUser().uid
-//                        }, function() {
-//                            defer.resolve(patients);
-//                        }, function() {
-//                            defer.reject();
-//                        });
-//
-//                    }, function() {
-//                        $location.path("/splash");
-//                    });
-//
-//                    return defer.promise;
-//                }]
+            templateUrl: 'app/feed/feed.tpl.html',
+            controller: 'FeedCtrl',
+            resolve: {
+                activities: ['ActivityModel', 'SessionService', '$q', function (ActivityModel, SessionService, $q) {
+                    var defer = $q.defer();
+
+                    var activities = ActivityModel.query({
+                        user_id: SessionService.currentUser.uid
+                    }, function () {
+                        console.log("in here", activities);
+                        defer.resolve(activities);
+                    }, function (error) {
+                        defer.reject();
+                    });
+
+                    return defer.promise;
+                }]
             }
         });
     }])
 
-    .controller('FeedCtrl', ['$scope', '$location', 'PatientModel', function ($scope, $location, PatientModel) {
+    .controller('FeedCtrl', ['$scope', '$location', 'activities', function ($scope, $location, activities) {
 //        $scope.patients = patients;
 
         // Load all patients
-        $scope.patients = PatientModel.index();
+        $scope.activities = activities;
+
+        console.log("activities are ", activities);
+
     }]);
 
